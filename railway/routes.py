d26673832ac66b0318680db199e73d47accfcb68
Gautam7702 <gomcisethia@gmail.com>
2022-12-20 23:23:32 +0530

    Defined the routes

--- railway/routes.py
+++ railway/routes.py
@@ -4,3 +4,11 @@ from flask import render_template
 @app.route('/')
 def home_page():
     return render_template('home.html')
+
+@app.route('/book-tickets',methods = ['GET','POST'])
+def book_tickets_page():
+    return render_template('book_tickets.html')
+
+@app.route('/add_train',methods = ['GET','POST'])
+def add_train_page():
+    return render_template('add_train.html')
